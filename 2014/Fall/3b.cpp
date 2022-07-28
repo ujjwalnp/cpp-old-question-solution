6ffb893a70fa5c0c3471f54610ee4e601003c665
Ujjwal Dhakal <dhakalujjwal99@gmail.com>
2022-07-28 23:39:00 +0545

    initial commit

---
+++ 2014/Fall/3b.cpp
@@ -0,0 +1,45 @@
+// WAP to concatenate two strings (name and address of a person) using the concept of containership.
+
+#include<bits/stdc++.h>
+using namespace std;
+
+// Class that is to be contained
+class FirstClass {
+    private :
+        char name[50], address[50];
+    
+    public : 
+        void input() {       // taking input from the user
+            cout<<"What is your name ?"<<endl;
+            cin>>name;
+            cout<<"What is your address ?"<<endl;
+            cin>>address;
+        }
+
+        string concat () {
+            strcat(name, address);
+            return name;
+        }
+        void print(){
+            cout<<"Final String = "<<name<<endl;
+        }
+};
+
+// Container Class
+class SecondClass {
+    // creating object of FirstClass
+    FirstClass f;
+    
+    public :
+        // constructor
+        SecondClass () {
+            f.input();
+            f.concat();
+            f.print();
+        }
+};
+
+int main () {
+    SecondClass s;
+    return 0;
+}
