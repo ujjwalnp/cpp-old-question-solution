2b1dfc34d7ebd7f7c4751f56429375b3a0f115cd
karkiayush <karkiayush619@gmail.com>
2022-07-31 12:37:28 +0545

    6th commit done by AK on july 31

---
+++ AayushKarki's/14fall3bContainership.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+class nm
+{
+public:
+    string name = "mike ";
+};
+class ad
+{
+public:
+    string address = "manchester";
+    nm n;
+    string concatenated = n.name + address;
+};
+int main()
+{
+    nm N;
+    ad A;
+    cout << "string 1 : " << N.name << endl;
+    cout << "string 2 : " << A.address << endl;
+    cout << "Concantenated string : " << A.concatenated << endl;
+}
