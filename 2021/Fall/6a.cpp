f20625db9644e032e713e8522d8a766876d568a0
Ujjwal Dhakal <dhakalujjwal99@gmail.com>
2022-08-02 23:46:28 +0545

    added 2021 Fall 6a

---
+++ 2021/Fall/6a.cpp
@@ -0,0 +1,47 @@
+// 
+
+#include<iostream>
+using namespace std;
+
+class Complex {
+    private :
+        int real, imaginary;
+        int real1, imaginary1;
+        int sum_real, sum_imaginary;
+    public :
+        Complex () {
+            real = 5;
+            imaginary = 7;
+        }
+        Complex (int real, int imaginary) {
+            this->real = real;
+            this->imaginary = imaginary;
+        }
+        Complex operator++() {
+            real = real+1;
+            imaginary = imaginary+1;
+        }
+        void input() {
+            cout<<"Enter another complex number\nreal = ";
+            cin>>real1;
+            cout<<"imaginary = ";
+            cin>>imaginary1;
+        }
+        Complex operator+ () {
+            sum_real = real + real1;
+            sum_imaginary = imaginary + imaginary1;
+        }
+        void display () {
+            cout<<"increament result \nreal = "<<real<<"\nimaginary = "<<imaginary<<endl;
+            cout<<"added complex number result : "<<sum_real<<" i "<<" + "<<sum_imaginary<<" j "<<endl;
+        }
+};
+int main () {
+    Complex c;
+    ++c;
+    c.input();
+    +c;
+    c.display();
+    return 0;
+
+}
